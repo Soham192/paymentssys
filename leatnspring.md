ca44f6b394bd80f1aac75a487f1107be399b98ed
Soham <savesoham5@gmail.com>
2025-04-26 11:26:55 +0530

    x

---
+++ leatnspring.md
@@ -0,0 +1,8 @@
+1.model is where we store the data or the objects/entities that we are going to use 
+it is connected to the repository class which where all the db code is written
+
+2.dependency injection using @Autowired annotation 
+
+3.In Spring, a Service is a class that holds your business logic — it's where you write code that connects your controller (HTTP layer) to your repository (DB layer).
+
+4.
---
+++ src/main/java/com/paymentsys/service/Userservice.java
@@ -0,0 +1,30 @@
+package com.paymentsys.service;
+import com.paymentsys.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import com.paymentsys.repository.UserRepository;
+import com.paymentsys.model.User;
+import com.paymentsys.repository.*;
+@Service
+public class Userservice {
+    @Autowired //dependency injection
+    private UserRepository userRepository;
+    public User registerUser(User newuser){
+        return userRepository.save(newuser);}
+//to validate if user is alreay present
+    public User authenticate_User(String email ,String password){
+        Optional<User> user = userRepository.findByemail(email);
+        //if email is found can register as a user and if not found return user not found 
+        if(user.isPresent()&&user.get().getpassword().equals(password)){//user.ispresent a method of Optional conatiner obejct
+            return user.get();
+
+
+        }else{
+
+        }
+    }
+    
+
+    
+}
